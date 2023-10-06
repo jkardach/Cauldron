9dc6281e5e755ad1e48f27bf0ed54ae5ed35392e
tkardach <tommy@kardach.com>
2023-10-06 10:11:46 -0700

    Add audio implementation

--- sample/led_effect.py
+++ sample/led_effect.py
@@ -1,19 +1,22 @@
-import led.led_effect as led
+import led.led_effect as led_effect
 import led.led_strip as led_strip
 import matplotlib.pyplot as plt
 import numpy as np
 from test_tools import TestEffect, NUM_PIXELS
+import time
 
 
 def test_mock_sine_wave():
     color0 = [3, 252, 11]
     color1 = [229, 245, 5]
 
-    sine_wave = led.SineWaveEffect(
+    sine_wave = led_effect.SineWaveEffect(
         color0, color1, oscillate=True, b=5, oscillation_speed_ms=1000
     )
     mock_strip = led_strip.MockStrip(NUM_PIXELS)
-    TestEffect.test_effect(mock_strip, sine_wave)
+    player = led_effect.MockEffectPlayer(mock_strip, sine_wave)
+    handle = player.play()
+    time.sleep(10)
 
 
 test_mock_sine_wave()
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='led',
-    version='0.0.1',
-    packages=find_packages('app'),
-    package_dir={'': 'app'},
-    author='Tommy Kardach',
-    author_email='tommy@kardach.com',
+    name="cauldron",
+    version="0.0.1",
+    packages=find_packages("app"),
+    package_dir={"": "app"},
+    author="Tommy Kardach",
+    author_email="tommy@kardach.com",
 )
